5c3aae829b2f5685ae9f88701b1f062d8b7033a7
Chris Thoburn <runspired@gmail.com>
2016-03-15 18:20:35 -0500

    better test

--- tests/acceptance/bug-trigger-test.js
+++ tests/acceptance/bug-trigger-test.js
@@ -1,42 +1,79 @@
 /* jshint ignore:start */
 import { test } from 'qunit';
+import Ember from 'ember';
 import moduleForAcceptance from 'bug-13071/tests/helpers/module-for-acceptance';
+
+const {
+  RSVP
+  } = Ember;
+
+const {
+  Promise
+  } = RSVP;
 
 moduleForAcceptance('Acceptance | bug trigger');
 
-const boundVisit = visit.bind(this);
-const boundCurrentURL = currentURL.bind(this);
+test(`visiting /bug-trigger-${i}`, function(assert) {
+  let runs = 5;
+  assert.expect(runs);
+  let done = assert.async();
 
-for (let i = 0; i < 5; i++) {
-  test(`visiting /bug-trigger-${i}`, function(assert) {
-    assert.expect(6);
-    boundVisit('/');
-    assert.equal(boundCurrentURL(), '/', 'we transitioned');
+  const setup = () => {};
+  const teardown = () => {};
 
-    andThen(function() {
-      boundVisit('/visit-1');
-      assert.equal(boundCurrentURL(), '/visit-1', 'we transitioned');
+  function run(i) {
+    return new Promise((resolve, reject) => {
+      function throwIf(a, b, c) {
+        if (a !== b) {
+          reject();
+          assert.equal(a, b, c);
+        }
+      }
+
+      visit('/');
+      throwIf(currentURL(), '/', `we transitioned ${i}`);
 
       andThen(function() {
-        boundVisit('/visit-2');
-        assert.equal(boundCurrentURL(), '/visit-2', 'we transitioned');
+        visit('/visit-1');
+        throwIf(currentURL(), '/visit-1', `we transitioned ${i}`);
 
         andThen(function() {
-          boundVisit('/visit-3');
-          assert.equal(boundCurrentURL(), '/visit-3', 'we transitioned');
+          visit('/visit-2');
+          throwIf(currentURL(), '/visit-2', `we transitioned ${i}`);
 
           andThen(function() {
-            boundVisit('/visit-4');
-            assert.equal(boundCurrentURL(), '/visit-4', 'we transitioned');
+            visit('/visit-3');
+            throwIf(currentURL(), '/visit-3', `we transitioned ${i}`);
 
             andThen(function() {
-              boundVisit('/visit-5');
-              assert.equal(boundCurrentURL(), '/visit-5', 'we transitioned');
+              visit('/visit-4');
+              throwIf(currentURL(), '/visit-4', `we transitioned ${i}`);
+
+              andThen(function() {
+                visit('/visit-5');
+                throwIf(currentURL(), '/visit-5', `we transitioned ${i}`);
+                assert.ok(`run passed ${i}`);
+                resolve();
+              });
             });
           });
         });
       });
     });
-  });
-}
+  }
+
+  let promises = new Array(runs);
+  let chain = promises.reduce(function(chain, v, i) {
+    return chain
+      .then(setup)
+      .then(() => {
+        return run(i);
+      })
+      .then(teardown);
+  }, Promise.resolve());
+
+  return chain.then(done);
+
+});
+
 /* jshint ignore:end */
